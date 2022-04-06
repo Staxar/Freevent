3c48912f6dd28816e05ae98a399c4397e21e0c56
Staxar <brzeszczot90@gmail.com>
2022-04-06 14:15:13 +0200

    Add Heder and variables styles

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a4742f1913bb4045"; }
+/******/ 		__webpack_require__.h = function() { return "1af17a911f3b3639"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/Layout/Header.jsx
+++ components/Layout/Header.jsx
@@ -4,7 +4,14 @@ export default function Header() {
   return (
     <section className="header">
       <div className="header">
-          
+          <div className="nav__menu">
+            <div className="nav__list grid">
+              <li className="nav__item">Home</li>
+              <li className="nav__item">Events</li>
+              <li className="nav__item">About</li>
+              <li className="nav__item">Contact</li>
+            </div>
+          </div>
       </div>
     </section>
   )
