2c8153eccfbd983d1d7629c0b510ee8649c10819
Staxar <brzeszczot90@gmail.com>
2022-04-06 11:00:49 +0200

    Create base for Freevent application

---
+++ components/Layout/Content.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Content() {
+  return (
+    <section className="content-container">
+        <div className="container">
+            
+        </div>
+    </section>
+  )
+}
---
+++ pages/_app.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from '../components/Layout/Header'
+import Footer from '../components/Layout/Footer'
+import Content from '../components/Layout/Content'
+import '../styles/index.scss'
+
+function App() {
+  return (
+    <div>    
+      <Header />
+      <Content />
+      <Footer />
+    </div>
+  )
+}
+export default App
