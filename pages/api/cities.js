163eea26fd80a0233f390003a071d33178332178
Staxar <brzeszczot90@gmail.com>
2022-04-13 17:24:22 +0200

    Fixed database connection, start create CitiesCard

--- pages/api/cities.js
+++ pages/api/cities.js
@@ -7,7 +7,14 @@ export default async function handler(req, res) {
 
   dbConnect();
 
-  if(method === "GET"){}
+  if(method === "GET"){
+    try {
+      const cities = await Cities.find();
+      res.status(200).json(cities);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  }
   if(method === "POST"){
     try {   
       const cities = await Cities.create(req.body);
