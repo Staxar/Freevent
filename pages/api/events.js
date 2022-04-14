24c1053149bf9f456e20916b8b4db03e098a1ba7
Staxar <brzeszczot90@gmail.com>
2022-04-14 15:54:15 +0200

    Add media queries, events model prepared, finish footer

---
+++ pages/api/events.js
@@ -0,0 +1,27 @@
+import Events from "../../models/Events";
+import dbConnect from "../../util/mongodb";
+
+ 
+export default async function handler(req, res) {
+  const { method } = req;
+
+  dbConnect();
+
+  if(method === "GET"){
+    try {
+      const events = await Events.find();
+      res.status(200).json(events);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  }
+  if(method === "POST"){
+    try {   
+      const events = await Events.create(req.body);
+      res.status(201).json(events);
+
+    }catch(err){
+    res.status(500).json(err);
+    }
+  }
+}
