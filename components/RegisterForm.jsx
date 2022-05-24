56ee3692b951c239f84fc6de48820939b083463b
Staxar <brzeszczot90@gmail.com>
2022-05-24 07:50:27 +0200

    Add register template

---
+++ components/RegisterForm.jsx
@@ -0,0 +1,50 @@
+import { Small_User, Lock_Closed, Facebook, Instagram, Tiktok } from './Icons/Icons'
+export default function RegisterForm() {
+  return (
+    <form className="login__form" name='register__form'>
+                    <div className="login__title">Sign Up</div>
+
+                    <div className="login__input_box">
+                        <div className="login__span">Username</div>
+                        <div className="login__flex">
+                            <span className='login__span_icon'><Small_User /></span>
+                            <input type="text" name='username' placeholder='Type your username' className='login__input_form'></input>
+                        </div>
+                    </div>
+
+                    <div className="login__input_box">
+                        <div className="login__span">Password</div>
+                        <div className="login__flex">
+                            <span className='login__span_icon'><Lock_Closed /></span>
+                            <input type="text" name='password' placeholder='Type your password' className='login__input_form'></input>
+                        </div>
+                    </div>
+                    <div className="login__input_box">
+                        <div className="login__span">Confirm Password</div>
+                        <div className="login__flex">
+                            <span className='login__span_icon'><Lock_Closed /></span>
+                            <input type="text" name='confirm_password' placeholder='Confirm your password' className='login__input_form'></input>
+                        </div>
+                    </div>                    
+
+                    <div className="login__form_button">
+                        <div className="login__form_button_wrap">
+                            <div className="login__form__button_box">
+                                <button className="login__button_login">Sign Up</button>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="login__text-center">Or Sign Up Using</div>
+                    <div className="login__providers">
+                        <Facebook />
+                        <Instagram />
+                        <Tiktok />
+                    </div>
+                    <div className="login__text-center">Back to login</div>
+                </form>
+  )
+}
+
+
+
--- models/Users.js
+++ models/Users.js
@@ -1,20 +1,33 @@
 import mongoose from "mongoose";
 
-const UsersSchema = new mongoose.Schema({
-  first_name: {
-    type: String,
-    required: true,
-    maxlength: 255,
-  },
-  second_name: {
-    type: String,
-    maxlength: 255,
-  },
-  age: {
-    type: Number,
-    required: true,
-    maxlength: 100,
-  },
-});
+const UsersSchema = new mongoose.Schema(
+  {
+    login: {
+      type: String,
+      required: true,
+      maxlength: 100,
+    },
+    name: {
+      type: String,
+      maxlength: 200,
+    },
+    email: {
+      type: String,
+      maxlength: 200,
+    },
+    age: {
+      type: Number,
+      maxlength: 100,
+    },
+    passwordSalt: {
+      type: String,
+      maxlength: 100,
+    },
+    passwordHash: {
+      type: String,
+      maxlength: 255,
+    },
+}
+);
 
-export default mongoose.models.Users || mongoose.model("Users", UsersSchema);
+export default mongoose.models.Users || mongoose.model("users", UsersSchema);
